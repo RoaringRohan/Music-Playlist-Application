dba311eef45613255806637869b6ee05d8fd3dcf
Rohan Datta <rdatta8@uwo.ca>
2022-12-07 09:49:43 -0500

    Attempting to connect to api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const {MongoClient} = require('mongodb');
 
-const url = "mongodb+srv://rdatta8:Rajatroro1!@se3316-webtech.lnvqrnd.mongodb.net/?retryWrites=true&w=majority";
+const url = "mongodb+srv://user:roots@se3316-webtech.lnvqrnd.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 
-const app = express();
+// Connecting to Database
 
 async function start() {
   try {
@@ -23,9 +23,19 @@ async function start() {
 
 start().catch(console.dir);
 
+// Prints all Databases
 async function listDatabases(client){
   databasesList = await client.db().admin().listDatabases();
 
   console.log("Databases:");
   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
+
+// API Connection and Endpoints
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
