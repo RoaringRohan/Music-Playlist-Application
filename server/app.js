8dd1140d4585952ea78464ac9f72855fd04bb35d
Rohan Datta <rdatta8@uwo.ca>
2023-01-02 16:11:28 -0500

    Adding npm packages to package.json files and connecting to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,9 +1,31 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
 const port = 3000;
 
-app.listen(port, () => {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useUnifiedTopology: true, 
+      useNewUrlParser: true
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+connectDB();
+
+app.use('/api/login', require('./routes/login'));
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
+});
